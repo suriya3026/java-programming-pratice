eb590767a2e5071ec1e24bf33976404554f7f8b3
SURIYA J <144776563+suriya3026@users.noreply.github.com>
2026-07-02 20:47:25 +0530

    Add files via upload

---
+++ arithmethic/task1.java
@@ -0,0 +1,26 @@
+package arithmethic;
+
+public class task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=10;
+			float b=20.f;
+			double c=10.3456;
+			char d='s';
+			String e="suriya";
+			boolean f=true;
+			
+			System.out.println(a+b);
+			System.out.println(c-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println(f);
+		
+
+	}
+
+}

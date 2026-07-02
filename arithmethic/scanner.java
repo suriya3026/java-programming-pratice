27873424e25a0c64c049d631f5a92629167e30ea
SURIYA J <144776563+suriya3026@users.noreply.github.com>
2026-07-02 20:46:18 +0530

    Add files via upload

---
+++ arithmethic/scanner.java
@@ -0,0 +1,33 @@
+package arithmethic;
+import java.util.Scanner;
+
+public class scanner 
+{
+	public static void main(String[] args)
+	{
+		Scanner ac=new Scanner(System.in);
+		System.out.println("COLLEGE APPLICATION FORM");
+		System.out.print("Enter your name :");
+		String a=ac.nextLine();
+		System.out.print("Enter your age :");
+		int b=ac.nextInt();
+		ac.nextLine();
+		System.out.print("Enter your gender :");
+		String c=ac.nextLine();
+		System.out.print("Enter your email id :");
+		String d=ac.nextLine();
+		System.out.print("Enter your mobile no :");
+		double g=ac.nextDouble();
+		System.out.print("Enter your 12th mark :");
+		double h=ac.nextDouble();
+		System.out.print("Enter Your 10th mark :");
+		double i=ac.nextDouble();
+		ac.nextLine();
+		System.out.print("Enter your course :");
+		String j=ac.nextLine();
+		
+		
+		
+	}
+
+}
